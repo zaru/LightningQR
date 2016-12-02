987a7537f4750f2779a646b6c0c7f1dfc33370f6
zaru@sakuraba <zarutofu@gmail.com>
2016-12-02 11:25:33 +0900

    init commit

---
+++ LightningQR/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  LightningQR
+//
+//  Created by hiro on 2016/12/02.
+//  Copyright © 2016年 zaru. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
