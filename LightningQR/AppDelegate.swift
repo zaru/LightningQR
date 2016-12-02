c62f9337609d14cc1e5920ad5938c3a37fb52aa5
zaru@sakuraba <zarutofu@gmail.com>
2016-12-02 13:10:07 +0900

    add pasteboard observer and URL validator

--- LightningQR/AppDelegate.swift
+++ LightningQR/AppDelegate.swift
@@ -9,9 +9,10 @@
 import Cocoa
 
 @NSApplicationMain
-class AppDelegate: NSObject, NSApplicationDelegate {
+class AppDelegate: NSObject, NSApplicationDelegate, Validator {
 
     var statusItem = NSStatusBar.system().statusItem(withLength: -2)
+    var changeCount: Int = 0
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
@@ -19,12 +20,61 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         if let button = statusItem.button {
             button.image = NSImage(named: "StatusImage")
         }
+        
+        beginObservingPasteboard()
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
+    
+    func beginObservingPasteboard() {
+        self.changeCount = NSPasteboard.general().changeCount
+        Timer.scheduledTimer(timeInterval: 1.0,
+                             target: self,
+                             selector: #selector(observePasteboard),
+                             userInfo: nil,
+                             repeats: true)
+    }
+    
+    func observePasteboard() {
+        let pboard = NSPasteboard.general()
+        if (pboard.changeCount > self.changeCount) {
+            pasteboardChanged(pboard: pboard)
+        }
+        self.changeCount = pboard.changeCount
+    }
+    
+    func pasteboardChanged(pboard:NSPasteboard) {
+        let str = pboard.string(forType: NSStringPboardType)
+        print(str)
+        print(validateURL(urlString: str!))
+        
+    }
 
 
+}
+
+
+protocol Validator {
+    func validateURL(urlString: String) -> Bool
+}
+
+extension Validator {
+    func validateURL(urlString: String) -> Bool {
+        var result = false
+        let types: NSTextCheckingResult.CheckingType = .link
+        let detector = try? NSDataDetector(types: types.rawValue)
+        guard let detect = detector else {
+            return result
+        }
+        let matches = detect.matches(in: urlString, options: .reportCompletion, range: NSMakeRange(0, urlString.characters.count))
+        for match in matches {
+            if let url = match.url {
+                result = true
+            }
+        }
+        return result
+    }
 }
 
